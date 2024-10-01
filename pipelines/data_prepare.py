b628aa3b4926d94e26180e9a8245c558d9c0b564
Ivan Golov <ivangolov@icloud.com>
2024-10-01 20:06:14 +0300

    zenml pipline ready

---
+++ pipelines/data_prepare.py
@@ -0,0 +1,94 @@
+from typing import Annotated
+import pandas as pd
+import numpy as np
+from zenml import pipeline, step, ArtifactConfig
+from src.transformations import feature_extractor, clear_columns, merge_columns, generate_embeddings
+from src.data import read_datastore, load_features, fetch_features
+from sklearn.pipeline import Pipeline
+
+@step(enable_cache=False)
+def extract() -> (
+    tuple[
+        Annotated[
+            pd.DataFrame,
+            ArtifactConfig(name="extracted_data", tags=["data_preparation"]),
+        ],
+        Annotated[str, ArtifactConfig(name="data_version", tags=["data_preparation"])],
+    ]
+):
+    """
+    Extract the sample data from the data store, and its version.
+    """
+    df, version = read_datastore()
+    print("Extracted from datastore", version)
+    return df, version
+
+@step(enable_cache=False)
+def transform(
+    df: pd.DataFrame) -> (
+    tuple[
+        Annotated[
+            pd.DataFrame,
+            ArtifactConfig(name="transformed_data", tags=["data_preparation"]),
+        ],
+        Annotated[
+            pd.Series, 
+            ArtifactConfig(name="target", tags=["data_preparation"])],
+    ]
+    ):
+    # Split to features and target
+    X = df.drop(columns=["target"])
+    y = df["target"]
+
+    all_features = X.columns.tolist()
+    new_feature_name = 'merged_text'
+
+    # Create the pipeline
+    transformation_pipeline = Pipeline([
+        ('extract_features', feature_extractor(features=all_features)),
+        ('clean_columns', clear_columns(features=all_features)),
+        ('merge_columns', merge_columns(features=all_features, new_feature_name=new_feature_name)),
+    ])
+    
+    # Fit and transform the data 
+    transformed_X = transformation_pipeline.fit_transform(X)
+    print("Transformation completed successfuly")
+    return transformed_X, y
+
+@step(enable_cache=False)
+def obtain_embeddings(
+    X: pd.DataFrame, embeder_path: str, column_name: str
+) -> (Annotated[
+        np.ndarray, 
+        ArtifactConfig(name="features_target", tags=["data_preparation"])
+    ]):
+    
+    # Generate embeddings
+    embeddings = generate_embeddings(embeder_path=embeder_path, X=X, column_name=column_name)
+    print("Embeddings generation completed")
+    return embeddings
+
+@step(enable_cache=False)
+def load(
+    X: np.ndarray, y: pd.Series, version: str
+) -> tuple[
+    Annotated[np.ndarray, ArtifactConfig(name="embedings", tags=["data_preparation"])],
+    Annotated[pd.Series, ArtifactConfig(name="target", tags=["data_preparation"])],
+]:
+    """
+    Load the features and target as artifact.
+    """
+    load_features(X, y, version=version)
+    print("Loaded into features store", version)
+    return X, y
+
+@pipeline()
+def prepare_data_pipeline():
+    df, version = extract()
+    X, y = transform(df)
+    X = obtain_embeddings(X=X, embeder_path='models/embedders/all-MiniLM-L6-v2',column_name='merged_text')
+    X, y = load(X, y, version)
+
+
+if __name__ == "__main__":
+    run = prepare_data_pipeline()
--- src/data.py
+++ src/data.py
@@ -9,16 +9,16 @@ def read_datastore(train=True) -> tuple[pd.DataFrame, str]:
     """
     Read the sample data.
     """
-    cfg = OmegaConf.load("../configs/data_description.yaml")
-    version = open("../configs/data_version.txt", "r").read().strip()
+    cfg = OmegaConf.load("configs/data_description.yaml")
+    version = open("configs/data_version.txt", "r").read().strip()
     
-    data_path = os.path.join('..', cfg.data.sample_train_path if train else cfg.data.sample_test_path)
+    data_path = os.path.join('', cfg.data.sample_train_path if train else cfg.data.sample_test_path)
     data = pd.read_csv(data_path)
     
     return data, version
     
 
-def load_features(X: np.array, y: pd.DataFrame, version: str) -> None:
+def load_features(X: np.ndarray, y: pd.Series, version: str) -> None:
     """
     Save the features_target and target as artifact.
     """
